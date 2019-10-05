9b32d4edbf3652d1180a2366544ce060d54b0023
Abhinav Pappu <apappu2@illinois.edu>
2019-10-05 16:13:16 -0500

    Setup router with blank home and login pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import Home from './pages/Home';
+import Login from './pages/Login';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Router>
+      {/* A <Switch> looks through its children <Route>s and renders the first one that matches the current URL. */}
+      <Switch>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
