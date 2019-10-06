5c9c7a74864b26e4f4ee5d261d0367457ffa022b
Abhinav Pappu <apappu2@illinois.edu>
2019-10-06 17:04:38 -0500

    Create intial layout for login and signup pages

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,7 +1,33 @@
 import React from "react";
+import { TextField, Button } from '@material-ui/core';
+
+import './styles.sass';
 
 export default class Login extends React.Component {
   render() {
-    return <p>Login</p>;
+    return (
+      <div className="login-page">
+        <div className="container">
+          <div className="login-form">
+            <div className="title">Login</div>
+            <TextField className="input" label="Email Address" fullWidth={true}/>
+            <TextField className="input" label="Password" type="password" fullWidth={true}/>
+            <div className="spacer"/>
+            <Button className="button" color="primary" variant="contained">Login</Button>
+          </div>
+
+          <div className="separator"/>
+
+          <div className="signup-form">
+            <div className="title">Sign Up</div>
+            <TextField className="input" label="Email Address" fullWidth={true}/>
+            <TextField className="input" label="Password" type="password" fullWidth={true}/>
+            <TextField className="input" label="Repeat Password" type="password" fullWidth={true}/>
+            <div className="spacer"/>
+            <Button className="button" color="primary" variant="contained">Sign Up</Button>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
