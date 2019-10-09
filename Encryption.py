b47e46df083f7a012062b14a57c4f5872f620e03
Tony Tan <tonytan2@illinois.edu>
2019-10-09 15:09:13 -0500

    finish __init__ function for encryption class

--- Encryption.py
+++ Encryption.py
@@ -1,9 +1,12 @@
 import base64
+import numpy as np
+import cryptography as cp
 #use the thing below in a class that takes in an image file
 
 class Ecryption:
+    """A class used for encryption"""
     def __init__(self, information):
-        self.info = np.array(information)
-    with open(information, "rb") as image_file:
-    encoded_string = base64.b64encode(image_file.read())
-    
+        with open(information, "rb") as image_file:
+            encoded_string = base64.b64encode(image_file.read())
+        self.info = encoded_string.decode
+    def encode(self):
