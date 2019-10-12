9e23bb80a2d04d185c8b18759fce4572e72b5b30
Aishani Dutta <adutta7@illinois.edu>
2019-10-12 00:30:59 -0500

    expanded post-login homepage

--- src/components/DocumentView/index.js
+++ src/components/DocumentView/index.js
@@ -4,6 +4,9 @@ import maleLicenseStock from "assets/maleLicenseStock.jpg"
 import passportClipart from "assets/passportClipart.jpg"
 import socialSecurityCartoon from "assets/socialSecurityCartoon.png"
 import birthCertificateCartoon from "assets/birthCertificateCartoon.png"
+import birthCertificateActual from "assets/birthCertificateActual.jpg"
+import actualFemaleLicense from "assets/actualFemaleLicense.jpg"
+import { Paper, TableRow, TableHead, TableCell, TableBody, Table } from '@material-ui/core';
 import "./style.sass"
 
 const typeToImage = {
@@ -18,15 +21,28 @@ export default class DocumentView extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            clickedDocument: "",
             documents: [
                 {
-                    type: "driver"
+                    type: "driver",
+                    img: actualFemaleLicense,
+                    parsedInfo: [
+                        { NAME: "JANE Q" },
+                        { ISS: "05/01/2016" },
+                        { EXP: "11/14/2020" },
+                    ],
                 },
                 {
-                    type: "passport"
+                    type: "birth",
+                    img: birthCertificateActual,
+                    parsedInfo: [
+                        {NAME: "EXAMPLE"},
+                        {SEX: "FEMALE"},
+                        {DOB: "SEPTEMBER 14, 1990"}
+                    ]
                 },
                 {
-                    type: "birth"
+                    type: "passport",
                 },
                 {
                     type: "social"
@@ -37,12 +53,39 @@ export default class DocumentView extends React.Component {
             ]
         }
     }
+    handleScrollClick(document) {
+        this.setState({
+            clickedDocument: document
+        })
+    }
     render() {
-        return(
+        return (
             <div className="document-view">
                 <div className="scroll">
-                    {this.state.documents.map(document => <img src={typeToImage[document.type]}/>)}
+                    {this.state.documents.map(document => (
+                        <img alt="Icon representing stored document"
+                            onClick={() => this.handleScrollClick(document)}
+                            src={typeToImage[document.type]} />
+                    ))}
                 </div>
+                {this.state.clickedDocument &&
+                    <div className="storedInfo">
+                        <img src={this.state.clickedDocument.img}></img>
+                        <Paper className="infoTable">
+                            <Table>
+                                <TableBody>
+                                    {this.state.clickedDocument.parsedInfo.map(item => (
+                                        <TableRow>
+                                            <TableCell className="description">{Object.entries(item)[0][0]}</TableCell>
+                                            <TableCell className="value">{Object.entries(item)[0][1]}</TableCell>
+                                        </TableRow>
+                                    )
+                                    )}
+                                </TableBody>
+                            </Table>
+                        </Paper>
+                    </div>
+                }
             </div>
         );
     }
