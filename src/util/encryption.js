3c83d7c44cec4b30ea14639af645c776e7474427
Tony Tan <tonytan2@illinois.edu>
2019-11-02 18:52:31 -0500

    create outline of tasks

---
+++ src/util/encryption.js
@@ -0,0 +1,20 @@
+//function that generates keys takes in (security questions) and password 
+//first hash the password then encrypt
+//- aes encryption with keys, return struct of encrypPrivateKey and publicKey;
+// use let or const instead of things like int 
+
+//encrypt with public key (publicKey, string "type", string "image", struct "parsedInfo" ) -> returns the encrypted strings as struct with keys type, image and parsedInfo
+//first line should be JSON.stringify(parsedInfo) so that my output
+
+//function to hashThePassword
+
+//decrypt with (HashOfPassword, encryptPrivateKey)
+
+//decrypt with (struct, actualPrivateKey) - > return same Struct but parsedInfo is struct from (JSON.parse(parsedInfoString))
+
+/** 
+export function BIHKk () {
+
+}
+*/
+
